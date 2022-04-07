8d2074197cb37df43c02f6d3fe36358bfd583011
Halle Countryman <hkcountryman@gmail.com>
2022-04-06 21:02:30 -0700

    Initial state of foo.c, foo.h

---
+++ foo.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "foo.h"
+
+person* create_person(char *name, char *phone) {
+    person* person = malloc(sizeof(person));
+    person->name = name;
+    strcpy(person->phone, phone);
+    return person;
+}
+
+void call(person *person) {
+    printf("Dialing (");
+    for (int i = 0; i < 10; i++) {
+        printf("%d", person->phone[i]);
+        switch (i) {
+        case 2:
+            printf(")");
+        case 5:
+            printf("-");
+        }
+    }
+    printf("...\n");
+    printf("Hello, %s here! Is this the person to whom I am speaking?", person->name);
+}
+
+int main (int argc, char **argv) {
+    person *p = create_person("Halle", "15551234567");
+    call(p);
+    return 0;
+}
---
+++ foo.h
@@ -0,0 +1,8 @@
+typedef struct {
+    char *name;
+    int phone[10];
+} person;
+
+person* create_person(char *name, char *phone);
+
+void call(person *person);
