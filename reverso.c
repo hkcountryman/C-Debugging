375fda1ecd0551eb5872ea6ff3224ee6898616f7
Halle Countryman <hkcountryman@gmail.com>
2022-04-07 12:12:49 -0700

    Update reverso and GDB.md

--- reverso.c
+++ reverso.c
@@ -5,10 +5,14 @@
 
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+void foo() {
     int a = 1;
     int b = 2;
     int c = a + b;
     printf("%d + %d = %d\n", a, b, c);
+}
+
+int main(int argc, char **argv) {
+    foo();
     return 0;
 }
